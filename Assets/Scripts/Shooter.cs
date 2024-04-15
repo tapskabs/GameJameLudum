41539126638cc49d3eaf81190def2c7b8f79e00a
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2024-04-15 14:31:35 +0200

    Shooter with gun

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -9,19 +9,49 @@ public class Shooter : MonoBehaviour
     public Transform firePoint; // The position from which the bullet will be fired
     public float bulletSpeed = 10f; // Speed of the bullet
     public float bulletLifetime = 2f; // Time until the bullet is destroyed after being fired
-
+    public int maxAmmo = 30;
+    public int currentAmmo = -1;
+    public float reloadTime = 2f;
+    private bool isReloading = false;
     // Update is called once per frame
+
+    private void Start()
+    {
+        if (isReloading)
+            return;
+        if (currentAmmo == -1)
+        {
+            currentAmmo = maxAmmo;
+        }
+    }
     void Update()
     {
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         // Check if the player presses the fire button (for example, the space key)
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Fire(); // Call the Fire method
         }
+
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading....");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 
     void Fire()
     {
+
+        currentAmmo--;
         // Instantiate a new bullet object at the firePoint position and rotation
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
