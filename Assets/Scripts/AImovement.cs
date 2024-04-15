eb688b4759f4a62bc2e8e278b22807e7491d9b4d
CFrance <calebfrance005@gmail.com>
2024-04-15 20:35:18 +0200

    Prefab Map, Enemy AI, Modular FPS Controller
    Prefab map imported and tweaked, enemy AI has been created. Using the AI agent and AI Mesh navigator the zombies plot a path to the player's position. The old FPS system was changed to the Modular FPS Controller plugin which honed in on the smoothness of the controls as well as adding crouch and zoom.

---
+++ Assets/Scripts/AImovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AImovement : MonoBehaviour
+{
+    public Transform playerTransforms;
+    public float maxTime = 1.0f;
+    public float minDistance = 1.0f;
+
+    NavMeshAgent agent;
+    Animator animator;
+    float timer = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0.0f) {
+            float sqDistance = (playerTransforms.position - agent.destination).sqrMagnitude;
+            if (sqDistance > minDistance*minDistance) {
+                agent.destination = playerTransforms.position;
+            }
+            timer = maxTime;
+        }
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
