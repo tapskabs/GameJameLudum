1ab2ccd6ff239be07585c95e19c1174771495184
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2024-04-15 12:37:56 +0200

    GunShooter
    The game shoots bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            Debug.Log("You hit" + collision.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bulletPrefab; // Reference to the bullet prefab
+    public Transform firePoint; // The position from which the bullet will be fired
+    public float bulletSpeed = 10f; // Speed of the bullet
+    public float bulletLifetime = 2f; // Time until the bullet is destroyed after being fired
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the player presses the fire button (for example, the space key)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire(); // Call the Fire method
+        }
+    }
+
+    void Fire()
+    {
+        // Instantiate a new bullet object at the firePoint position and rotation
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Access the Rigidbody component of the bullet object
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        // Check if the Rigidbody component exists
+        if (rb != null)
+        {
+            // Add force to the bullet in the direction of its forward vector (from firePoint)
+            rb.AddForce(firePoint.forward * bulletSpeed, ForceMode.Impulse);
+        }
+        else
+        {
+            Debug.LogError("Bullet prefab does not have a Rigidbody component!");
+        }
+
+        // Destroy the bullet after bulletLifetime seconds
+        Destroy(bullet, bulletLifetime);
+    }
+}
